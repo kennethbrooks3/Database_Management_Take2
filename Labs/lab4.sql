33001b6d45bb405c06a41512328ec7095109c4a6
kennethbrooks3 <kenneth.brooks3@gmail.com>
2014-10-13 18:06:48 -0400

    Re commit of lap 4
    trying to setup git on multiple machines and somehow my initial push for
    lab 4 was deleted.

---
+++ Labs/lab4.sql
@@ -0,0 +1,80 @@
+--1--
+select distinct city
+from agents
+where aid in 
+        	(select distinct aid
+        	from orders 
+        	where cid in 
+                		(select cid
+                		from customers
+                		where name = 'Tiptop'
+                		)
+          )
+
+
+--2--
+select distinct pid
+from orders
+where aid in 
+        	(select aid
+        	from orders
+        	where cid in 
+                		(select cid
+                		from customers
+                		where city = 'Kyoto'
+                		)
+          )
+
+--3--
+select cid, name
+from customers
+where cid not in 
+            	(select cid
+            	from orders
+            	where aid = 'a04'
+            	)
+
+--4--
+select cid, name
+from customers
+where cid in 
+        	(
+        	(select distinct cid
+        	from orders
+        	where pid ='p01')
+		      intersect
+        	(select cid
+        	from orders
+        	where pid = 'p07')
+        	)
+
+--5--
+select distinct pid
+from orders
+where cid in
+        	(select cid
+        	from orders
+        	where aid = 'a04'
+        	)
+
+--6--
+select name, discount
+from customers
+where cid in 
+        	(select cid
+        	from orders
+        	where aid in
+                		(select aid
+                		from agents
+                		where city = 'Dallas' or city = 'Newark'
+                		)
+          )
+
+--7--
+select *
+from customers
+where city != 'Dallas'and city != 'Kyoto'and discount in 
+        	(select discount
+        	from customers
+        	where city = 'Dallas' or city = 'Kyoto'
+        	)
